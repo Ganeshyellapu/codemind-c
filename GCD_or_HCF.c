28af1dc881ebb9ee19ebf846a540407ae783e4b1
Ganeshyellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-04-20 23:14:39 +0530

    #include<stdio.h> int main() {     int n,m,min,i,hcf;     scanf("%d%d",&n,&m);     min=(n<m)?n:m;     for(i=1;i<=min;i++)     {         if(n%i==0 && m%i==0)         {             hcf=i;         }     }     printf("%d",hcf); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,min,i,hcf;
+    scanf("%d%d",&n,&m);
+    min=(n<m)?n:m;
+    for(i=1;i<=min;i++)
+    {
+        if(n%i==0 && m%i==0)
+        {
+            hcf=i;
+        }
+    }
+    printf("%d",hcf);
+}
