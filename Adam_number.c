e3fbc0f2efebfdc0cc4af30a402e723ef1b06f8e
Ganeshyellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-04-23 01:11:09 +0530

    #include<stdio.h> int main() { 	int n,q,k,res=0,x,y,t,z=0,r; 	scanf("%d",&n); 	k=n; 	q=n*n; 	while(k) 	{ 		r=k%10; 		res=res*10+r; 		k=k/10; 	} 	x=res*res; 	t=x; 	while(t) 	{ 		y=t%10; 		z=z*10+y; 		t=t/10; 	} 	if(z==q) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} }

---
+++ Adam_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int n,q,k,res=0,x,y,t,z=0,r;
+	scanf("%d",&n);
+	k=n;
+	q=n*n;
+	while(k)
+	{
+		r=k%10;
+		res=res*10+r;
+		k=k/10;	
+	}
+	x=res*res;
+	t=x;
+	while(t)
+	{
+		y=t%10;
+		z=z*10+y;
+		t=t/10;
+	}
+	if(z==q)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+}
